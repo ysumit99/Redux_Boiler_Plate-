789da115753c4d1b08008943dad93dc24455a3df
ysumit99 <ysumit99@gmail.com>
2018-08-21 00:16:00 +0530

    Basics of React DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // create a new component. this component should produce some HTML
 
-const App = function() {
+const App = () => {
     return <div>Hi!</div>;
 }
 
 
 // Take this component's generated HTML and put it on the DOM
 
+ReactDOM.render(<App />, document.querySelector('.container'));
