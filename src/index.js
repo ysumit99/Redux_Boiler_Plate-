c7d1605d3a691a9f618800a4389bf91100307d48
ysumit99 <ysumit99@gmail.com>
2018-08-22 21:29:41 +0530

    youtube search api and started with search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyA6fVi2Fv8qsbj63es9bFS0z3h-CWyN_o4';
+
+
 // create a new component. this component should produce some HTML
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 
