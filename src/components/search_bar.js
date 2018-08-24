62bb637a50d83d4dea693aa0551f51e24a388fbc
ysumit99 <ysumit99@gmail.com>
2018-08-25 00:22:50 +0530

    React state and this.setState method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
-    render() {
-        return <input />;
+class SearchBar extends   Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: 'starting Value'};
     }
+
+    render() {
+        return ( 
+            <div>
+                <input 
+                value = {this.state.term}
+                onChange = { event => this.setState({ term: event.target.value }) }/>
+            </div>
+        );
+    }
+
 }
 
 
