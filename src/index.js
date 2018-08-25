ba4b0f4e5802d12ae46922a2b981524a747f7c88
ysumit99 <ysumit99@gmail.com>
2018-08-26 00:59:32 +0530

    YTSearch youtube search API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyA6fVi2Fv8qsbj63es9bFS0z3h-CWyN_o4';
+
 
 
 // create a new component. this component should produce some HTML
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component  {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+
+        YTSearch({key: API_KEY, term: 'federer'}, (videos) => {
+            this.setState({ videos }); //ES6 syntax
+            // this.setState( { videos: videos }); //ES5 syntax
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+    
 }
 
 
