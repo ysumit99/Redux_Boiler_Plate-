b245edfbb54b9f42c54d05ed23568d301c0cb72d
ysumit99 <ysumit99@gmail.com>
2018-08-27 23:30:05 +0530

    Video description added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import videoList from './components/video_list';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyA6fVi2Fv8qsbj63es9bFS0z3h-CWyN_o4';
 
 
@@ -18,7 +18,7 @@ class App extends Component  {
         this.state = { videos: [] };
 
 
-        YTSearch({key: API_KEY, term: 'federer'}, (videos) => {
+        YTSearch({key: API_KEY, term: '2fact3'}, (videos) => {
             this.setState({ videos }); //ES6 syntax
             // this.setState( { videos: videos }); //ES5 syntax
         });
@@ -28,6 +28,7 @@ class App extends Component  {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail  video = {this.state.videos[0]} />
                 <VideoList  videos={ this.state.videos} />
             </div>
         );
